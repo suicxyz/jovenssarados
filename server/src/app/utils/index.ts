226b471fec1e87b581d323f9980557ee687f935a
suicxyz <suicxyz@proton.me>
2023-02-13 21:34:26 -0300

    Remove unused function

--- server/src/app/utils/index.ts
+++ server/src/app/utils/index.ts
@@ -1,9 +1,5 @@
 import { tz, Moment } from "moment-timezone";
 
-export const sum = (x: number, y: number): number => {
-	return x + y;
-};
-
 export const BrazilTimezone = (): Moment => {
 	return tz(Date.now(), "America/Sao_Paulo");
 };
