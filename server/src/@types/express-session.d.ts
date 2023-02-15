1f0889b806a2a9bc279b7910f9ada6845e65599e
suicxyz <suicxyz@proton.me>
2023-02-15 19:16:08 -0300

    Declare session.login module

--- server/src/@types/express-session.d.ts
+++ server/src/@types/express-session.d.ts
@@ -2,10 +2,10 @@ import "express-session";
 import mongoose from "src/app/database";
 
 declare module "express-session" {
-	interface SessionData {
+	interface SessionData extends mongoose.Document {
 		login?: {
-			_id?: String;
-			user?: String;
+			_id?: mongoose.Schema.Types.ObjectId;
+			name?: String;
 			email?: String;
 			password?: String;
 			profile_pic?: String;
@@ -13,7 +13,7 @@ declare module "express-session" {
 			phone_number?: String;
 			cpf?: String;
 			geo_location?: mongoose.Schema.Types.ObjectId;
-			registered_events?: mongoose.Schema.Types.ObjectId;
+			registered_events?: Array<mongoose.Schema.Types.ObjectId>;
 			is_moderator?: Boolean;
 			created_at?: Date;
 		};
