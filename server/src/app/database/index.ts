403983aecd28599a6a9de18b17fb6d6efd98df1c
suicxyz <suicxyz@proton.me>
2023-02-17 22:19:19 -0300

    Change connection from local to cluster

--- server/src/app/database/index.ts
+++ server/src/app/database/index.ts
@@ -1,7 +1,10 @@
 import mongoose from "mongoose";
+require("dotenv").config();
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/agenda-js");
+mongoose.connect(
+	`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hdalw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+);
 mongoose.Promise = global.Promise;
 
 export default mongoose;
