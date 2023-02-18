2ed82f2bc1e64b3864dcf612aec3c4e97cbda6b9
suicxyz <suicxyz@proton.me>
2023-02-17 21:55:05 -0300

    Fix JSON response on errors

--- server/src/app/controllers/user.controller.ts
+++ server/src/app/controllers/user.controller.ts
@@ -6,33 +6,28 @@ import { cpf } from "cpf-cnpj-validator";
 export default new (class UserController {
 	async list(req: Request, res: Response): Promise<Response> {
 		try {
-			const users = await User.find();
+			const users = await User.find().populate("registered_events");
 
 			return res.status(200).json({ status: "OK", users });
 		} catch (e) {
-			return res.status(400).json({ status: "ERROR", message: e.message });
+			return res.status(400).json({ status: "ERROR", message: "ONC" });
 		}
 	}
 
 	async show(req: Request, res: Response): Promise<Response> {
 		const { id } = req.params;
 
-		if (!id)
-			return res
-				.status(400)
-				.json({ status: "ERROR", message: "User ID is not specified in URL." });
+		if (!id) return res.status(400).json({ status: "ERROR", message: "INS." });
 
 		try {
 			const user = await User.findById(id);
 
 			if (!user)
-				return res
-					.status(400)
-					.json({ status: "ERROR", message: "User not found." });
+				return res.status(400).json({ status: "ERROR", message: "DNF" });
 
 			return res.status(200).json({ status: "OK", user });
 		} catch (e) {
-			return res.status(400).json({ status: "ERROR", message: e.message });
+			return res.status(400).json({ status: "ERROR", message: "ONC" });
 		}
 	}
 
@@ -42,21 +37,16 @@ export default new (class UserController {
 		try {
 			if (
 				await User.findOne({
-					$or: [
-						{ email: body.email },
-						{ cpf: body.cpf }
-					]
+					$or: [{ email: body.email }, { cpf: body.cpf }],
 				})
 			) {
-				return res
-					.status(400)
-					.json({ status: "ERROR", message: "User already exist." });
+				return res.status(400).json({ status: "ERROR", message: "DAE" });
 			}
 
 			if (!cpf.isValid(body.cpf))
 				return res.status(400).json({
 					status: "ERROR",
-					message: "The specified CPF is not valid.",
+					message: "FNV",
 				});
 
 			const user = await User.create(body);
@@ -65,7 +55,7 @@ export default new (class UserController {
 
 			return res.status(201).json({ status: "OK", user });
 		} catch (e) {
-			return res.status(400).json({ status: "ERROR", message: e.message });
+			return res.status(400).json({ status: "ERROR", message: "ONC" });
 		}
 	}
 
@@ -76,21 +66,21 @@ export default new (class UserController {
 		if (!id)
 			return res.status(401).json({
 				status: "ERROR",
-				message: "Could not find user with the specified ID in URL.",
+				message: "INS",
 			});
 
 		try {
 			if (!(await User.findById(id)))
 				return res.status(401).json({
 					status: "ERROR",
-					message: "User does not exist.",
+					message: "DNF",
 				});
 
 			const user = await User.findByIdAndUpdate(id, body, { new: true });
 
 			return res.status(200).json({ status: "OK", user });
 		} catch (e) {
-			return res.status(401).json({ status: "ERROR", message: e.message });
+			return res.status(401).json({ status: "ERROR", message: "ONC" });
 		}
 	}
 
@@ -98,23 +88,20 @@ export default new (class UserController {
 		console.log(req);
 		const { id } = req.params;
 
-		if (!id)
-			return res
-				.status(400)
-				.json({ status: "ERROR", message: "User ID is not specified in URL." });
+		if (!id) return res.status(400).json({ status: "ERROR", message: "INS" });
 
 		try {
 			if (!(await User.findById(id)))
 				return res.status(400).json({
 					status: "ERROR",
-					message: "Could not find user with the provided ID.",
+					message: "DNF",
 				});
 
 			await User.findByIdAndDelete(id);
 
-			return res.status(200).json({ status: "OK" });
+			return res.status(200).json({ status: "OK", message: "DD" });
 		} catch (e) {
-			return res.status(400).json({ status: "ERROR", message: e.message });
+			return res.status(400).json({ status: "ERROR", message: "ONC" });
 		}
 	}
 })();
