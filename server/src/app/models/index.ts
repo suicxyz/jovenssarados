8fe6bf4d77a0bd54287c71f7d351dd64af2b68bf
suicxyz <suicxyz@proton.me>
2023-02-20 20:48:42 -0300

    :recycle: refactor: change file import

--- server/src/app/models/index.ts
+++ server/src/app/models/index.ts
@@ -1,5 +1,5 @@
 export { default as User } from "./user.model";
 export { default as Event } from "./event.model";
-export { default as UserEvent } from "./userevent.model";
+export { default as UserEvent } from "./user-event.model";
 export { default as Location } from "./location.model";
 export { default as EventLocation } from "./eventlocation.model";
