4ffae1344d8b746e1c58ff74e59a164719088768
suicxyz <suicxyz@proton.me>
2023-02-20 18:45:47 -0300

    Add QRCode generator service

---
+++ server/src/app/services/ticket.ts
@@ -0,0 +1,20 @@
+import qrcode from "qrcode";
+
+import { Event, User } from "@models";
+
+export default async (eventId: String, userId: String): Promise<any> => {
+	try {
+		const user = await User.findById(userId);
+		if (!user) return { status: "ERROR", message: "DNF[User]" };
+
+		const event = await Event.findById(eventId);
+		if (!event) return { status: "ERROR", message: "DNF[Event]" };
+
+		const codeContent = `${user._id}+${event._id}+${Date.now()}`;
+
+		const code: string = await qrcode.toDataURL(codeContent);
+		return { image: code, text: codeContent };
+	} catch (e) {
+		return { status: "ERROR", message: "ONC" };
+	}
+};
