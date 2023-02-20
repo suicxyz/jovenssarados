ee36f7ffd88ce47c0492601c6e670a40faf0d319
suicxyz <suicxyz@proton.me>
2023-02-20 19:43:07 -0300

    Add ConvertToBase64 function

--- server/src/app/utils/index.ts
+++ server/src/app/utils/index.ts
@@ -1,5 +1,14 @@
 import { tz, Moment } from "moment-timezone";
+import fs from "fs";
 
 export const BrazilTimezone = (): Moment => {
 	return tz(Date.now(), "America/Sao_Paulo");
+};
+
+export const ConvertToBase64 = (file: string) => {
+	const bitmap = fs.readFileSync(file, {
+		encoding: "base64",
+	});
+
+	return bitmap;
 };
