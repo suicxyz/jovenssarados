36567497c859f69a9c52fb6aa8e460b620d96149
suicxyz <suicxyz@proton.me>
2023-02-20 23:41:21 -0300

    :sparkles: feat: authorization middleware fully implemented

--- server/src/app/middlewares/auth.middleware.ts
+++ server/src/app/middlewares/auth.middleware.ts
@@ -1,12 +1,42 @@
 import { Request, Response, NextFunction } from "express";
 
+import * as config from "@config";
+
 export default async function (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) {
-	if (!(req.session.login || req.session.login._id))
-		return res.status(401).json({ status: "ERROR", message: "ANA" });
+	const { permissions } = config.permissions;
+	const session = req.session.login;
 
-	next();
+	for (const item of permissions) {
+		const method: string = item.split("[")[1].split("]")[0];
+		const route: string = item.split("]")[1].split("=")[0];
+		const level: string = item.split("=")[1];
+
+		if (req.method == method.toUpperCase() && req.url == route) {
+			if (level == "0") {
+				next();
+			} else if (level == "1") {
+				if (!session)
+					return res
+						.status(401)
+						.json({ status: "ERROR", message: "ANA[NoSession]" });
+				else next();
+			} else if (level == "2") {
+				if (!session) {
+					return res
+						.status(401)
+						.json({ status: "ERROR", message: "ANA[NoSession]" });
+				} else {
+					if (!session.is_moderator)
+						return res
+							.status(401)
+							.json({ status: "ERROR", message: "ANA[NotModerator]" });
+					else next();
+				}
+			}
+		}
+	}
 }
