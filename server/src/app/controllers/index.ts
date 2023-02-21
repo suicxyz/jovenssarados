df8bbd716637f3052990a1aee305903cb0911a10
suicxyz <suicxyz@proton.me>
2023-02-21 00:02:47 -0300

    :sparkles: feat: add import

--- server/src/app/controllers/index.ts
+++ server/src/app/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as User } from "./user.controller";
 export { default as Event } from "./event.controller";
 export { default as UserEvent } from "./user-event.controller";
+export { default as Auth } from "./auth.controller";
